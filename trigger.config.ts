a110b592456bb4ec7d032818bf2a49e95ec845dc
Manoj Singh Negi <www.msnegi@gmail.com>
2025-08-04 22:40:34 +0530

    updated

--- trigger.config.ts
+++ trigger.config.ts
@@ -20,3 +20,24 @@ export default defineConfig({
   },
   dirs: ["./src/trigger"],
 });
+
+// import { defineConfig } from "@trigger.dev/sdk/v3";
+
+// export default defineConfig({
+//   project: "proj_qekzrcjswkrfaurtclhu", // Just a local placeholder
+//   apiUrl: "http://localhost:8030", // Your local listener
+//   runtime: "node",
+//   logLevel: "log",
+//   maxDuration: 3600,
+//   retries: {
+//     enabledInDev: true,
+//     default: {
+//       maxAttempts: 3,
+//       minTimeoutInMs: 1000,
+//       maxTimeoutInMs: 10000,
+//       factor: 2,
+//       randomize: true,
+//     },
+//   },
+//   dirs: ["./src/trigger"],
+// });
